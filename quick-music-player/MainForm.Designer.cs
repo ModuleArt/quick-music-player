f376f61bb31a50ad2a6ac69686652fdd8f4d0976
Jake Volynko <volynko.ua@gmail.com>
2020-01-13 21:38:00 +0200

    v1.0.0

---
+++ quick-music-player/MainForm.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace quick_music_player
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.openButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.prevButton = new System.Windows.Forms.ToolStripButton();
+			this.playButton = new System.Windows.Forms.ToolStripButton();
+			this.pauseButton = new System.Windows.Forms.ToolStripButton();
+			this.nextButton = new System.Windows.Forms.ToolStripButton();
+			this.volumeButton = new System.Windows.Forms.ToolStripButton();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.artist = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.album = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.duration = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+			this.volumeSlider = new System.Windows.Forms.TrackBar();
+			this.currentSlider = new System.Windows.Forms.TrackBar();
+			this.volumeLabel = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.currentLabel = new System.Windows.Forms.ToolStripLabel();
+			this.loopButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.volumeSlider)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.currentSlider)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openButton,
+            this.toolStripSeparator1,
+            this.prevButton,
+            this.playButton,
+            this.pauseButton,
+            this.nextButton,
+            this.volumeButton,
+            this.volumeLabel,
+            this.toolStripSeparator2,
+            this.loopButton,
+            this.toolStripSeparator3,
+            this.currentLabel});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+			this.toolStrip1.Size = new System.Drawing.Size(484, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// openButton
+			// 
+			this.openButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.openButton.Image = ((System.Drawing.Image)(resources.GetObject("openButton.Image")));
+			this.openButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.openButton.Name = "openButton";
+			this.openButton.Size = new System.Drawing.Size(23, 22);
+			this.openButton.Text = "Add media";
+			this.openButton.ToolTipText = "Open";
+			this.openButton.Click += new System.EventHandler(this.openButton_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+			// 
+			// prevButton
+			// 
+			this.prevButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.prevButton.Enabled = false;
+			this.prevButton.Image = ((System.Drawing.Image)(resources.GetObject("prevButton.Image")));
+			this.prevButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.prevButton.Name = "prevButton";
+			this.prevButton.Size = new System.Drawing.Size(23, 22);
+			this.prevButton.Text = "Prev";
+			this.prevButton.ToolTipText = "Prev";
+			this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+			// 
+			// playButton
+			// 
+			this.playButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.playButton.Enabled = false;
+			this.playButton.Image = ((System.Drawing.Image)(resources.GetObject("playButton.Image")));
+			this.playButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.playButton.Name = "playButton";
+			this.playButton.Size = new System.Drawing.Size(23, 22);
+			this.playButton.Text = "Play";
+			this.playButton.Click += new System.EventHandler(this.playButton_Click);
+			// 
+			// pauseButton
+			// 
+			this.pauseButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.pauseButton.Enabled = false;
+			this.pauseButton.Image = ((System.Drawing.Image)(resources.GetObject("pauseButton.Image")));
+			this.pauseButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.pauseButton.Name = "pauseButton";
+			this.pauseButton.Size = new System.Drawing.Size(23, 22);
+			this.pauseButton.Text = "Pause";
+			this.pauseButton.Visible = false;
+			this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+			// 
+			// nextButton
+			// 
+			this.nextButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.nextButton.Enabled = false;
+			this.nextButton.Image = ((System.Drawing.Image)(resources.GetObject("nextButton.Image")));
+			this.nextButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.nextButton.Name = "nextButton";
+			this.nextButton.Size = new System.Drawing.Size(23, 22);
+			this.nextButton.Text = "Next";
+			this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+			// 
+			// volumeButton
+			// 
+			this.volumeButton.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.volumeButton.CheckOnClick = true;
+			this.volumeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.volumeButton.Image = ((System.Drawing.Image)(resources.GetObject("volumeButton.Image")));
+			this.volumeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.volumeButton.Name = "volumeButton";
+			this.volumeButton.Size = new System.Drawing.Size(23, 22);
+			this.volumeButton.Text = "Volume";
+			this.volumeButton.CheckedChanged += new System.EventHandler(this.volumeButton_CheckedChanged);
+			// 
+			// listView1
+			// 
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.number,
+            this.fileName,
+            this.title,
+            this.artist,
+            this.album,
+            this.duration});
+			this.listView1.FullRowSelect = true;
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(12, 65);
+			this.listView1.Name = "listView1";
+			this.listView1.ShowItemToolTips = true;
+			this.listView1.Size = new System.Drawing.Size(460, 484);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// number
+			// 
+			this.number.Text = "#";
+			this.number.Width = 30;
+			// 
+			// fileName
+			// 
+			this.fileName.Text = "File";
+			this.fileName.Width = 147;
+			// 
+			// artist
+			// 
+			this.artist.Text = "Artist";
+			this.artist.Width = 72;
+			// 
+			// album
+			// 
+			this.album.Text = "Album";
+			this.album.Width = 72;
+			// 
+			// title
+			// 
+			this.title.Text = "Title";
+			this.title.Width = 72;
+			// 
+			// duration
+			// 
+			this.duration.Text = "Length";
+			this.duration.Width = 45;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Multiselect = true;
+			// 
+			// volumeSlider
+			// 
+			this.volumeSlider.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.volumeSlider.AutoSize = false;
+			this.volumeSlider.LargeChange = 20;
+			this.volumeSlider.Location = new System.Drawing.Point(368, 28);
+			this.volumeSlider.Maximum = 100;
+			this.volumeSlider.Name = "volumeSlider";
+			this.volumeSlider.Size = new System.Drawing.Size(104, 31);
+			this.volumeSlider.SmallChange = 2;
+			this.volumeSlider.TabIndex = 2;
+			this.volumeSlider.TickFrequency = 10;
+			this.volumeSlider.Value = 50;
+			this.volumeSlider.Visible = false;
+			this.volumeSlider.ValueChanged += new System.EventHandler(this.volumeSlider_ValueChanged);
+			// 
+			// currentSlider
+			// 
+			this.currentSlider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.currentSlider.AutoSize = false;
+			this.currentSlider.Enabled = false;
+			this.currentSlider.Location = new System.Drawing.Point(12, 28);
+			this.currentSlider.Maximum = 100;
+			this.currentSlider.Name = "currentSlider";
+			this.currentSlider.Size = new System.Drawing.Size(460, 31);
+			this.currentSlider.TabIndex = 3;
+			this.currentSlider.TickFrequency = 25;
+			this.currentSlider.Scroll += new System.EventHandler(this.currentSlider_Scroll);
+			// 
+			// volumeLabel
+			// 
+			this.volumeLabel.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.volumeLabel.Name = "volumeLabel";
+			this.volumeLabel.Size = new System.Drawing.Size(29, 22);
+			this.volumeLabel.Text = "50%";
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+			// 
+			// currentLabel
+			// 
+			this.currentLabel.Name = "currentLabel";
+			this.currentLabel.Size = new System.Drawing.Size(72, 22);
+			this.currentLabel.Text = "00:00 / 00:00";
+			// 
+			// loopButton
+			// 
+			this.loopButton.CheckOnClick = true;
+			this.loopButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.loopButton.Image = ((System.Drawing.Image)(resources.GetObject("loopButton.Image")));
+			this.loopButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.loopButton.Name = "loopButton";
+			this.loopButton.Size = new System.Drawing.Size(23, 22);
+			this.loopButton.Text = "Loop song";
+			this.loopButton.CheckedChanged += new System.EventHandler(this.loopButton_CheckedChanged);
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 561);
+			this.Controls.Add(this.volumeSlider);
+			this.Controls.Add(this.currentSlider);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.toolStrip1);
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Quick Music Player";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.volumeSlider)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.currentSlider)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton openButton;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripButton prevButton;
+		private System.Windows.Forms.ToolStripButton playButton;
+		private System.Windows.Forms.ToolStripButton nextButton;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader fileName;
+		private System.Windows.Forms.ToolStripButton pauseButton;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ToolStripButton volumeButton;
+		private System.Windows.Forms.BindingSource bindingSource1;
+		private System.Windows.Forms.TrackBar volumeSlider;
+		private System.Windows.Forms.ColumnHeader duration;
+		private System.Windows.Forms.ColumnHeader number;
+		private System.Windows.Forms.ColumnHeader title;
+		private System.Windows.Forms.ColumnHeader artist;
+		private System.Windows.Forms.ColumnHeader album;
+		private System.Windows.Forms.TrackBar currentSlider;
+		private System.Windows.Forms.ToolStripLabel volumeLabel;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripLabel currentLabel;
+		private System.Windows.Forms.ToolStripButton loopButton;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+	}
+}
+
---
+++ quick-music-player/MainForm.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Timers;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace quick_music_player
+{
+	public partial class MainForm : Form
+	{
+		private List<string> songs = new List<string>();
+		private int currentSong = 0;
+		private WindowsMediaPlayer wmplayer = new WindowsMediaPlayer();
+		private System.Timers.Timer currentSliderTimer = new System.Timers.Timer();
+		private int nextReadyCounter = 0;
+		private bool loopSong = false;
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			listView1.MouseDoubleClick += new MouseEventHandler(listView1_MouseDoubleClick);
+
+			wmplayer.PositionChange += new _WMPOCXEvents_PositionChangeEventHandler(wmplayer_PositionChange);
+			wmplayer.PlayStateChange += new _WMPOCXEvents_PlayStateChangeEventHandler(wmplayer_PlayStateChange);
+
+			currentSliderTimer.Interval = 1000;
+			currentSliderTimer.Elapsed += new ElapsedEventHandler(currentSliderTimer_Elapsed);
+
+			volumeSlider.Value = Properties.Settings.Default.Volume;
+		}
+
+		private void wmplayer_PlayStateChange(int newState)
+		{
+			if (wmplayer.playState == WMPPlayState.wmppsPaused || wmplayer.playState == WMPPlayState.wmppsStopped)
+			{
+				pauseButton.Visible = false;
+				playButton.Visible = true;
+				currentSliderTimer.Stop();
+			}
+			else if (wmplayer.playState == WMPPlayState.wmppsPlaying)
+			{
+				pauseButton.Visible = true;
+				playButton.Visible = false;
+				currentSliderTimer.Start();
+			}
+			else if (wmplayer.playState == WMPPlayState.wmppsMediaEnded)
+			{
+				nextReadyCounter = 0;
+				if (loopSong)
+				{
+					playSong(currentSong);
+				}
+				else
+				{
+					nextSong();
+				}
+			}
+			else if (wmplayer.playState == WMPPlayState.wmppsReady)
+			{
+				nextReadyCounter++;
+				if (nextReadyCounter == 2)
+				{
+					play();
+				}
+			}
+		}
+
+		private void currentSliderTimer_Elapsed(object sender, EventArgs e)
+		{
+			int newValue = 0;
+			try
+			{
+				newValue = Convert.ToInt32(wmplayer.controls.currentPosition * 100 / wmplayer.currentMedia.duration);
+			}
+			catch
+			{
+
+			}
+
+			currentSlider.Invoke((MethodInvoker)(() =>
+				currentSlider.Value = newValue
+			));
+			currentLabel.GetCurrentParent().Invoke((MethodInvoker)(() =>
+				currentLabel.Text = wmplayer.controls.currentPositionString + " / " + wmplayer.currentMedia.durationString
+			));
+		}
+
+		private void wmplayer_PositionChange(double oldPosition, double newPosition)
+		{
+			int newValue = Convert.ToInt32(newPosition * 100 / wmplayer.currentMedia.duration);
+			currentSlider.Value = newValue; 
+		}
+
+		private void openButton_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				if (openFileDialog1.FileNames.Length > 0)
+				{
+					stop();
+
+					int lastCount = songs.Count;
+
+					ListViewItem[] lvis = new ListViewItem[openFileDialog1.FileNames.Length];
+					for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+					{
+						songs.Add(openFileDialog1.FileNames[i]);
+
+						IWMPMedia mediaInfo = wmplayer.newMedia(openFileDialog1.FileNames[i]);
+
+						string fileName = Path.GetFileName(openFileDialog1.FileNames[i]);
+
+						int index = lastCount + i;
+
+						lvis[i] = new ListViewItem(new string[] {
+							index.ToString(),
+							fileName,
+							mediaInfo.getItemInfo("Title"),
+							mediaInfo.getItemInfo("Artist"),
+							mediaInfo.getItemInfo("Album"),
+							mediaInfo.durationString
+						});
+					}
+					listView1.Items.AddRange(lvis);
+					playSong(0);
+
+					prevButton.Enabled = true;
+					nextButton.Enabled = true;
+					playButton.Enabled = true;
+					pauseButton.Enabled = true;
+					currentSlider.Enabled = true;
+				}
+			}
+		}
+
+		private void playSong(int songIndex)
+		{
+			listView1.Items[currentSong].Font = new Font(listView1.Font, FontStyle.Regular);
+			currentSong = songIndex;
+			listView1.Items[currentSong].Font = new Font(listView1.Font, FontStyle.Bold);
+
+			wmplayer.URL = songs[songIndex];
+			play();
+		}
+
+		private void stop()
+		{
+			wmplayer.controls.stop();
+		}
+
+		private void nextSong()
+		{
+			if (currentSong == songs.Count - 1)
+			{
+				playSong(0);
+			}
+			else
+			{
+				playSong(currentSong + 1);
+			}
+		}
+
+		private void prevSong()
+		{
+			if (currentSong == 0)
+			{
+				playSong(songs.Count - 1);
+			}
+			else
+			{
+				playSong(currentSong - 1);
+			}
+		}
+
+		private void play()
+		{
+			wmplayer.controls.play();
+		}
+
+		private void pause()
+		{
+			wmplayer.controls.pause();
+		}
+
+		private void playButton_Click(object sender, EventArgs e)
+		{
+			play();
+		}
+
+		private void pauseButton_Click(object sender, EventArgs e)
+		{
+			pause();
+		}
+
+		private void volumeButton_CheckedChanged(object sender, EventArgs e)
+		{
+			volumeSlider.Visible = volumeButton.Checked;
+
+			if (volumeButton.Checked)
+			{
+				currentSlider.Width = 350;
+			}
+			else
+			{
+				currentSlider.Width = listView1.Width;
+			}
+		}
+
+		private void volumeSlider_ValueChanged(object sender, EventArgs e)
+		{
+			wmplayer.settings.volume = volumeSlider.Value;
+			volumeLabel.Text = volumeSlider.Value + "%";
+
+			Properties.Settings.Default.Volume = volumeSlider.Value;
+			Properties.Settings.Default.Save();
+		}
+
+		private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			ListViewHitTestInfo hit = listView1.HitTest(e.Location);
+
+			if (hit.Item != null)
+			{
+				playSong(hit.Item.Index);
+			}
+		}
+
+		private void prevButton_Click(object sender, EventArgs e)
+		{
+			prevSong();
+		}
+
+		private void nextButton_Click(object sender, EventArgs e)
+		{
+			nextSong();
+		}
+
+		private void currentSlider_Scroll(object sender, EventArgs e)
+		{
+			double newPosition = Convert.ToDouble(currentSlider.Value * wmplayer.currentMedia.duration / 100);
+			wmplayer.controls.currentPosition = newPosition;
+		}
+
+		private void loopButton_CheckedChanged(object sender, EventArgs e)
+		{
+			loopSong = loopButton.Checked;
+		}
+	}
+}
